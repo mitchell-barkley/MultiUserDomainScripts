e89af352b28ba9aa55aca30958f58a0c92dc257d
Mitchell <138683597+mitchell-barkley@users.noreply.github.com>
2024-07-12 00:32:57 -0230

    Update T1_lockpick.js

--- T1_lockpick.js
+++ T1_lockpick.js
@@ -1,29 +1,13 @@
-function (
-    z,
-    r,
-    l,
-    t = r.t.call,
-    d = [...Array(98).keys()],
-    s = s => s[0].split(1),
-    c = s`red1orange1yellow1lime1green1cyan1blue1purple`,
-    p = {
-        E: s`release1open1unlock`,
-        d,
-        e: d.filter(d => d > 1 & [2, 3, 5, 7].every(p => d == p | d % p)),
-        l: s`vc2c7q1cmppiq1tvfkyq1uphlaw16hh8xw1xwz7ja1sa23uw172umy0`
-    }
-) {
-    r = t(p)
-    for (;
-        !/nn/.test(r);
-        z = (p[l[0]] || (
-            /_/.test(l) ? [
-                l[3] > 1 ?
-                c[(c.indexOf(z) + 4 + +l[l[3] > 2 ? 11 : 1]) % 8]
-                : z.length
-            ]
-            : c
-        )).find(v => !/th/.test((p[l] = v, r = t(p))))
+function (prevlock, response, currlock, target = response.t.call, digits = [...Array(98).keys()], s = s => s[0].split(-),
+    colours = s`red-orange-yellow-lime-green-cyan-blue-purple`,
+    params = {EZ: s`release1open1unlock`, digits, 
+              prime: digits.filter(digits => digits > 1 & [2, 3, 5, 7].every(params => digits == params | digits % params)), 
+              locket: s`vc2c7q-cmppiq-tvfkyq-uphlaw-6hh8xw-xwz7ja-sa23uw-72umy0`}) {
+
+    response = target(params)
+    for (!/nn/.test(response);
+        prevlock = (p[currlock[0]] || (/_/.test(currlock) ? [currlock[3] > 1 ? c[(c.indexOf(prevlock) + 4 + currlock[currlock[3] > 2 ?
+                    11 : 1]) % 8] : prevlock.length] : c)).find(v => !/th/.test((params[currlock] = v, response = target(params))))
     )
-        [,l] = /.*`N(.*?)`.*$/.exec(r)
+    [,currlock] = /.*`N(.*?)`.*$/.exec(response)
 }
